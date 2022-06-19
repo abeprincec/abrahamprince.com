5ae75a6845c9c4c56f0c21b6bc18ff933fa00548
Abraham Prince <abeprincec@gmail.com>
2022-06-19 19:27:41 -0400

    nav color

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -7,7 +7,7 @@ export default function Nav() {
   const inactiveClasses = `hover:bg-gray-900 filter-saturate hover:bg-opacity-5 dark:hover:bg-white dark:text-white  hover:text-gray-1000 dark:hover:text-gray-100 text-tertiary`;
 
   return (
-    <nav className="fixed top-0  w-full py-2 bg-white border-b border-gray-400 dark:border-opacity-10 border-opacity-20 md:bg-opacity-70 dark:bg-gray-900 dark:bg-opacity-40 filter-blur">
+    <nav className="fixed top-0  w-full py-2 bg-white border-b border-gray-400  border-opacity-20 bg-opacity-90 dark:bg-black dark:bg-opacity-90  dark:border-opacity-10 filter-blur">
       <ul className="flex max-w-4xl  gap-6 mx-auto h-12"></ul>
     </nav>
   );
